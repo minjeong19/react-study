0353e538ca3a40df6119975da56788936eccc5ad
minjeong <kmjeongk@gmail.com>
2023-05-01 23:03:24 +0900

    useState사용

--- src/components/NewExpense.jsx
+++ src/components/NewExpense.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import "../style/newExpense.scss";
 import ExpenseForm from "./ExpenseForm";
 
-export default function NewExpense() {
+export default function NewExpense(props) {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    // console.log(expenseData);
+    props.onAddExpense(expenseData);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 }
